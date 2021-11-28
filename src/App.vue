47e802f42101a7c55c536749bf3070b169346b96
Alexjimin <53390752+Alexjimin@users.noreply.github.com>
2021-11-28 21:09:45 +0900

    initialize

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <Topper></Topper>
+  <Banner></Banner>
+</template>
+
+<script>
+import Topper from "@/components/Topper.vue";
+import Banner from "@/components/Banner.vue";
+
+export default {
+  name: "App",
+  components: { Topper, Banner },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Material+Icons");
+@font-face {
+  font-family: "NEXON Lv1 Gothic OTF";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/NEXON Lv1 Gothic OTF.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
+@import "./assets/style.css";
+.unselectable {
+  user-select: none;
+}
+#app {
+  font-family: "NEXON Lv1 Gothic OTF", "malgun gothic", "sans serif";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 100vw;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+}
+
+body {
+  width: 100vw;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="banner">
+    <div class="banner-content-container">
+      <div class="banner-content">
+        <img v-bind:src="BannerImages[BannerPosition]" alt="a" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+  data() {
+    return {
+      BannerPosition: 0,
+      BannerImages: [
+        "https://rs.nxfs.nexon.com/bannerusr/21/11/C7x117154551704.png",
+        "https://rs.nxfs.nexon.com/bannerusr/21/11/24JL25105114402.jpg",
+        "https://rs.nxfs.nexon.com/bannerusr/21/11/wc8j25173925006.png",
+        "https://rs.nxfs.nexon.com/bannerusr/21/11/hZtP25115339941.png",
+        "https://rs.nxfs.nexon.com/bannerusr/21/11/WVF824175510292.jpg",
+        "https://rs.nxfs.nexon.com/bannerusr/21/11/LeTC22093916848.png",
+        "https://rs.nxfs.nexon.com/bannerusr/21/11/L1By25131511602.jpg",
+        "https://rs.nxfs.nexon.com/bannerusr/21/11/24JL25105114402.jpg",
+      ],
+      SetBannerPosition: window.setInterval(() => {
+        this.BannerPosition += 1;
+        this.BannerPosition = this.BannerPosition % this.BannerImages.length;
+        console.log(1);
+      }, 10000),
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
